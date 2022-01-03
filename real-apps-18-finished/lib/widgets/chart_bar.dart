c87bf55711bb9cc15605f9c41683c601a5bd98c7
Eijgermans, P.B.J. (Peter) <p.eijgermans11@gmail.com>
2022-01-03 19:33:46 +0100

    changed layout chart_bar

--- real-apps-18-finished/lib/widgets/chart_bar.dart
+++ real-apps-18-finished/lib/widgets/chart_bar.dart
@@ -37,7 +37,7 @@ class ChartBar extends StatelessWidget {
                 child: Container(
                   decoration: BoxDecoration(
                     color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10),
+                    borderRadius: BorderRadius.circular(15),
                   ),
                 ),
               ),
